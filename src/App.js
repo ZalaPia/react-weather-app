4b7f88952b06dcf6349757f100db181547d2cb9b
ZalaPia <zalapia.lipicnik@gmail.com>
2026-01-08 14:54:51 +0100

    Add Footer

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import './App.css';
 import Menu from "./Components/Menu/Menu";
 import Logo from "./Components/Logo/Logo";
 import Header from "./Components/Header/Header";
-
+import Footer from "./Components/Footer/Footer";
 export function App() {
 
     const logo = new URL('./assets/logo.svg', import.meta.url);
+    const love = new URL('./assets/love.svg', import.meta.url);
 
     return (
         <>
-            <Header><Logo title="Weather by VicBox" url={logo} />
-            <Menu items={[
-                { name: "Github", link: "/" },
-                { name: "About", link: "#about"}
-            ]}/>
+            <Header>
+                <Logo title="Weather by VicBox" url={logo} />
+                <Menu items={[
+                    { name: "Github", link: "/" },
+                    { name: "About", link: "#about"}
+                ]}/>
             </Header>
+            <Footer>
+                <span>Made with by <img src={love} alt="love"/>Victor Alvarado</span>
+            </Footer>
         </>
     );
 }
