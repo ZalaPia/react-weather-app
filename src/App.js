0c3eac824fe4d1bf250f10c7b159f14d186de51e
ZalaPia <zalapia.lipicnik@gmail.com>
2026-01-09 08:18:58 +0100

    Add Weather

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import Wrapper from "./Components/Card/Wrapper/Wrapper";
 import Card from "./Components/Card/Card";
+import Weather from "./Components/Weather/Weather";
 
 export function App() {
 
@@ -22,9 +23,11 @@ export function App() {
                 ]}/>
             </Header>
             <Wrapper>
-                <Card></Card>
                 <Card>
-                    <img src={add} alt="add" />
+                    <Weather/>
+                </Card>
+                <Card>
+                    <img src={add} alt="add"/>
                     <p>Add New Location</p>
                 </Card>
             </Wrapper>
